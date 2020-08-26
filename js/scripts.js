732e50356d07ae961392d6ca44e0ee6d711f567e
Brian Harvey <brian.harv3y@gmail.com>
2020-08-26 09:23:22 -0700

    initial commit
    Co-authored-by: Diego Salazar <salazardiego343@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+// Business Logic 
+
+
+
+
+
+// Randomizer Logic
+function getRandomInt(min, max) {
+  min = Math.ceil(1);
+  max = Math.floor(7);
+  return Math.floor(Math.random() * (7 - 1) + 1); //The maximum is exclusive and the minimum is inclusive
+}
+
+
+// User Interface Logic
